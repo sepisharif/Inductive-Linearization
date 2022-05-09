ff97466ab7024735639aa7c15888d7d685259e01
Sepideh Sharif <s_sharif996@yahoo.com>
2022-05-10 11:53:11 +1200

    Initials for the adaptive step size calculation

---
+++ GetInitials2.m
@@ -0,0 +1,11 @@
+%% Initials for the adaptive step size calculation
+
+%% Setting for the fixed step size
+ %GeneralInitials
+ %time=[minTime:stepsize: maxTime];
+
+%% Setting for the adaptive step size
+ AdaptiveTime
+ 
+%% Genaral setting
+c0=zeros(size(time));
