e814f64d9f2e33af745fc45c379488a66a9d5f78
Sepideh Sharif <s_sharif996@yahoo.com>
2022-05-10 11:48:18 +1200

    Initials

---
+++ TimeInitial.m
@@ -0,0 +1,8 @@
+%% Initials
+GeneralInitials
+stepsize =0.25;
+time_init=[minTime:1: maxTime];
+StepSizeVector=zeros(size(time_init));
+TimeVector1=zeros(size(time_init));
+c0_init=zeros(size(time_init));
+y0_update_init=c0_init;
